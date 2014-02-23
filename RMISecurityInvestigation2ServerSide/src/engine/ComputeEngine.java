0a1da0eb8cab33e5c1e3ad4152fd0480d150199d
mahabrashid <mabz@donotemail.me>
2014-02-23 22:05:31 +0000

    Added all of interfaces, clientside and serverside implementation.

---
+++ RMISecurityInvestigation2ServerSide/src/engine/ComputeEngine.java
@@ -0,0 +1,39 @@
+package engine;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import compute.Compute;
+import compute.Task;
+
+public class ComputeEngine implements Compute {
+
+    public ComputeEngine() {
+        super();
+    }
+
+    public <T> T executeTask(Task<T> t) {
+        return t.execute();
+    }
+
+    public static void main(String[] args) {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        
+        try {
+            Registry registry = LocateRegistry.getRegistry();
+            String name = "Compute";
+            Compute engine = new ComputeEngine();
+            Compute stub =
+                (Compute) UnicastRemoteObject.exportObject(engine, 0);
+            System.out.println(stub.toString());
+            registry.rebind(name, stub);
+            System.out.println("ComputeEngine bound");
+        } catch (Exception e) {
+            System.err.println("ComputeEngine exception:");
+            e.printStackTrace();
+        }
+    }
+}
